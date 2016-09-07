49061b5ca51d01a504dd77bbfdced180c1b17bf9
rjmacarthy <richardmacarthy@hotmail.com>
2016-09-07 22:53:58 +0100

    Bourbon

--- src/containers/Footer.vue
+++ src/containers/Footer.vue
@@ -1,4 +1,25 @@
-<template>
-  <footer>Footer Container</footer>
+<template lang="jade">
+	footer Footer Container
 </template>
 
+<script>
+	export default {
+	  
+	  data () {
+	    return {
+	      
+	    }
+	  },
+	  created : function ()  {
+	     
+	  }
+	}
+</script>
+
+<style scoped lang="sass">
+   @import "~bourbon/app/assets/stylesheets/bourbon";
+   * 
+   	color: red;
+   	background-color: red;
+   	@include background(linear-gradient(red, green) left repeat);
+</style>
